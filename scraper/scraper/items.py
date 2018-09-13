426bc1f3f086b392d1fab092fb0f34615675f751
Edwin Julaton <julatonedwinv@gmail.com>
2018-09-13 14:33:59 +0800

    add additional request for users

--- scraper/scraper/items.py
+++ scraper/scraper/items.py
@@ -8,7 +8,7 @@
 import scrapy
 
 
-class ScrapperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class Client(scrapy.Item):
+    id = scrapy.Field()
+    first_name = scrapy.Field()
+    last_name = scrapy.Field()
--- scraper/scraper/spiders/clients_spider.py
+++ scraper/scraper/spiders/clients_spider.py
@@ -1,11 +1,19 @@
-import scrapy
 from selenium import webdriver
+
+import requests
+import scrapy
+
+from scrapy.loader import ItemLoader
+from scraper.items import Client
+
 
 class ClientSpider(scrapy.Spider):
     name = "client"
 
     url = 'https://business101.resurva.com/account-admin/accounts/clients/'
-    login_url = 'https://business101.resurva.com/account-admin/accounts/clients/'
+    get_client_booking_url = 'https://business101.resurva.com/' +\
+        '/index/client-profile'
+
     login_user = 'edwin@baseup.co'
     login_password = 'skudd1993'
 
@@ -24,8 +32,32 @@ class ClientSpider(scrapy.Spider):
         return cookies
 
     def start_requests(self):
-        yield scrapy.Request(url=self.url, callback=self.after_login, cookies=self.get_cookies())
+        yield scrapy.Request(url=self.url,
+                             callback=self.after_login,
+                             cookies=self.get_cookies())
 
     def after_login(self, response):
+        user_ids = []
         for li in response.css('ul#clients-listing > li'):
-            self.logger.info(li.xpath('//div[starts-with(@class, "head")]'));
+            user_ids.append(li.css('li > div::attr(class)')
+                    .extract_first().split()[2].split('-')[1])
+
+        # Get users cookies
+        jar = requests.cookies.RequestsCookieJar()
+        for cookie in self.get_cookies():
+            jar.set(cookie['name'], cookie['value'],
+                    domain=cookie['domain'], path=cookie['path'])
+
+        # Request users data
+        r = requests.get(self.get_client_booking_url,
+                         params={'id': user_ids[0]}, cookies=jar)
+        # data = r.json()
+
+        # client = ItemLoader(item=Client(), response=response)
+        # client.add_value('id', data['id'])
+        # client.add_value('first_name', data['first_name'])
+        # client.add_value('last_name', data['last_name'])
+
+        # return client.load_item()
+        self.logger.info(r.json())
+        return r.json()
